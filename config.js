102771b477b1ede4cfc38ec3bb20e5aaf59cbf78
anilkumaricc <anilkumaricc@gmail.com>
2025-11-12 14:50:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
-// config.js
 window.ADCA = {
-  ENDPOINT: "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxx/exec",
-  PRIMARYKEY: "primaryEa7f4c2b",
-  ADMINKEY: "admin83c1d",
-  USE_SHEET_QUESTIONS: true,    // true => Sheet API, false => JSON files
-  JSON_PATHS: {
-    MAIN: (series, set) => `sets/Series_${series}_Set${String(set).padStart(3,"0")}.json`,
-    HW:   (series, set) => `hw/HW_Series_${series}_Set${String(set).padStart(3,"0")}.json`,
-    CW:   (series, set) => `hw/CW_Series_${series}_Set${String(set).padStart(3,"0")}.json`
-  },
-  TOKEN_TTL_SEC: 600,
-  DUP_WINDOW_MIN: 3
+ENDPOINT: "https://script.google.com/macros/s/AKfycbyC_kGdWjTuMOm0-blHhqjyn4mq_iJKkhTYr6zMj_NMVQd0EZgcgfM2ztjNWCoRW3q2/exec",
+PRIMARYKEY: "primaryEa7f4c2b",
+ADMINKEY: "admin83c1d",
+USE_SHEET_QUESTIONS: true,
+JSON_PATHS: {
+MAIN: (series, set) => sets/Series_${series}_Set${String(set).padStart(3,"0")}.json,
+HW: (series, set) => hw/HW_Series_${series}_Set${String(set).padStart(3,"0")}.json,
+CW: (series, set) => hw/CW_Series_${series}_Set${String(set).padStart(3,"0")}.json
+},
+TOKEN_TTL_SEC: 600,
+DUP_WINDOW_MIN: 3
 };
