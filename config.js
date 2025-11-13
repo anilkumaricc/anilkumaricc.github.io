015a14ebf978e5baa08885be641b29e93400133f
anilkumaricc <anilkumaricc@gmail.com>
2025-11-13 18:27:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 window.ADCA = {
   // Your deployed Apps Script Web App (use the /exec URL)
-  ENDPOINT: "https://script.google.com/macros/s/AKfycbyC_kGdWjTuMOm0-blHhqjyn4mq_iJKkhTYr6zMj_NMVQd0EZgcgfM2ztjNWCoRW3q2/exec",
+  ENDPOINT: "https://script.google.com/macros/s/AKfycbxiQK2dwe-sAQLQZfyz8skiTmzaA6s_S9pDLJZPh75NhRg8zYnmCN9I1YkB5xejptjC/exec",
 
   // MUST match backend SCRIPT_PROPS.PRIMARY_KEY & ADMIN_KEY
   PRIMARY_KEY: "primary_Ea7f4c2b",
@@ -15,3 +15,4 @@ window.ADCA = {
   TOKEN_TTL_SEC: 600,
   DUP_WINDOW_MIN: 3
 };
+
